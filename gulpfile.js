d9d350c04ded96eb1dbbc08a68dfb617429906aa
Jorge Moreno <alterebro@gmail.com>
2021-02-06 11:58:29 +0100

    Switch gulp-header for gulp-wrapper

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,31 @@ const gulp      = require("gulp");
 const babel     = require("gulp-babel");
 const uglify    = require("gulp-uglify");
 const rename    = require("gulp-rename");
-const header    = require("gulp-header");
 const markdown  = require('gulp-markdown');
 const wrapper   = require('gulp-wrapper');
 
-const pkg = require('./package.json');
-const banner = [
-        '/*!',
-        '<%= pkg.name %> v<%= pkg.version %>',
-        '<%= pkg.license %> (c) <%= new Date().getFullYear() %> <%= pkg.author %>',
-        '<%= pkg.homepage %>',
-        '*/',
-        ''
-    ].join('\n');
+// ~~~~~~~~~~~~~~~~~~
+// Build default
+const pkg = require("./package.json");
+const credits = `/*!
+${pkg.name} v${pkg.version}
+${pkg.license} (c) ${new Date().getFullYear()} ${pkg.author}
+${pkg.homepage}
+*/`;
 
 gulp.task("default", function() {
     return gulp.src("src/*.js")
         .pipe(babel({ presets: ["@babel/preset-env"] }))
         .pipe(uglify())
-        .pipe(header(banner, { pkg : pkg } ))
+        .pipe(wrapper({
+            header: credits + '\n',
+        }))
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("dist"));
 });
 
-
+// ~~~~~~~~~~~~~~~~~~
+// Build Docs
 const tpl = {
     header : `<!DOCTYPE html>
     <html lang="en">
